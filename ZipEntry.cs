dotnet/android-libzipsharp

commit ffffd815b6ca2fb316d925c3242d4a7e580e630f

Allow to open encrypted archives (#29)
Add the password string parameter in the method ExtractAll (ZipArchive.cs) and the method Extract(ZipEntry)
Add the private method OpenArchive which calls the native method zip_fopen_index_encrypted if the password parameter is filled. Otherwise, use the zip_fopen_index

ZipEntry.cs
@@ -230,6 +230,7 @@ namespace Xamarin.Tools.Zip
 			}
 			OnExtract (args);
 		}
+
 		/// <summary>
 		/// Extract this entry in directory specified by <paramref name="destinationDir"/>, optionally changing the entry's name to the
 		/// one given in <paramref name="destinationFileName"/>. The destination file is opened using mode specified in the
@@ -240,7 +241,8 @@ namespace Xamarin.Tools.Zip
 		/// <param name="outputFileMode">Output file mode.</param>
 		/// <param name="useNativeFileName">Make sure that the file name is converted to the operating system
 		/// native format before extracting</param>
-		public string Extract (string destinationDir = null, string destinationFileName = null, FileMode outputFileMode = FileMode.Create, bool useNativeFileName = false)
+		/// <param name="password">Password of the ZipEntry</param>
+		public string Extract (string destinationDir = null, string destinationFileName = null, FileMode outputFileMode = FileMode.Create, , bool useNativeFileName = false, string password = null)
 		{
 			destinationDir = destinationDir?.Trim ();
 			if (String.IsNullOrEmpty (destinationDir))
@@ -262,7 +264,7 @@ namespace Xamarin.Tools.Zip
 				OperationFlags flags = OperationFlags.None;
 				IntPtr file = IntPtr.Zero;
 				try {
-					file = Native.zip_fopen_index (archive.ArchivePointer, Index, flags);
+					file = OpenArchive(password, flags);
 					if (file == IntPtr.Zero)
 						throw archive.GetErrorException ();
 					DoExtract (file, path, outputFileMode, args);
@@ -412,6 +414,18 @@ namespace Xamarin.Tools.Zip
 			if (valid.HasFlag (field))
 				return value ();
 			return deflt;
+		}
+
+		IntPtr OpenArchive(string password, OperationFlags flags)
+		{
+			IntPtr file;
+
+			if (!String.IsNullOrWhiteSpace(password))
+				file = Native.zip_fopen_index_encrypted(archive.ArchivePointer, Index, flags, password);
+			else
+				file = Native.zip_fopen_index(archive.ArchivePointer, Index, flags);
+
+			return file;
 		}
 	}
 }

ZipArchive.cs
@@ -282,12 +282,14 @@ namespace Xamarin.Tools.Zip
 		/// </summary>
 		/// <returns>The all.</returns>
 		/// <param name="destinationDirectory">Destination directory.</param>
-		public void ExtractAll (string destinationDirectory = null)
+		/// <param name="password">Password of the ZipEntry</param>
+		public void ExtractAll(string destinationDirectory = null, string password = null)
 		{
-			foreach (ZipEntry ze in this) {
+			foreach (ZipEntry ze in this)
+			{
 				if (ze == null)
 					continue;
-				ze.Extract (destinationDirectory);
+				ze.Extract(destinationDirectory, password);
 			}
 		}
 

commit 44de300d48ac49effa74405850b75cd4216c4fdd

Fix ZipEntry.Extract() declaration.
Context: https://jenkins.mono-project.com/job/xamarin-android-pr-builder-release/137/

Commit ffffd815 introduced some compilation errors:

	ZipEntry.cs(245,134): error CS1031: Type expected
	ZipEntry.cs(245,134): error CS1001: Identifier expected

Remove the offending `, ,` so that libZipSharp can compile.

ZipEntry.cs
@@ -242,7 +242,7 @@ namespace Xamarin.Tools.Zip
 		/// <param name="useNativeFileName">Make sure that the file name is converted to the operating system
 		/// native format before extracting</param>
 		/// <param name="password">Password of the ZipEntry</param>
-		public string Extract (string destinationDir = null, string destinationFileName = null, FileMode outputFileMode = FileMode.Create, , bool useNativeFileName = false, string password = null)
+		public string Extract (string destinationDir = null, string destinationFileName = null, FileMode outputFileMode = FileMode.Create, bool useNativeFileName = false, string password = null)
 		{
 			destinationDir = destinationDir?.Trim ();
 			if (String.IsNullOrEmpty (destinationDir))