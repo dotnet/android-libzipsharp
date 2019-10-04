9ae4a85d3d30092207c49b54234cdfabc85439bc
Dean Ellis <dellis1972@googlemail.com>
2019-10-04 15:11:28 +0100

    Add New Unit Test

---
+++ LibZipSharp.UnitTest/ZipArchiveEx.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Xamarin.Tools.Zip;
+
+namespace Xamarin.Tools.Zip.Ex {
+	public class ZipArchiveEx : IDisposable {
+
+		public static int ZipFlushLimit = 50;
+
+		ZipArchive zip;
+		string archive;
+
+		public ZipArchive Archive {
+			get { return zip; }
+		}
+
+		public ZipArchiveEx (string archive) : this (archive, FileMode.CreateNew)
+		{
+		}
+
+		public ZipArchiveEx (string archive, FileMode filemode)
+		{
+			this.archive = archive;
+			zip = ZipArchive.Open (archive, filemode);
+		}
+
+		public void Flush ()
+		{
+			if (zip != null) {
+				zip.Close ();
+				zip.Dispose ();
+				zip = null;
+			}
+			zip = ZipArchive.Open (archive, FileMode.Open);
+		}
+
+		string ArchiveNameForFile (string filename, string directoryPathInZip)
+		{
+			if (string.IsNullOrEmpty (filename)) {
+				throw new ArgumentNullException (nameof (filename));
+			}
+			string pathName;
+			if (string.IsNullOrEmpty (directoryPathInZip)) {
+				pathName = Path.GetFileName (filename);
+			} else {
+				pathName = Path.Combine (directoryPathInZip, Path.GetFileName (filename));
+			}
+			return pathName.Replace ("\\", "/");
+		}
+
+		void AddFiles (string folder, string folderInArchive, CompressionMethod method)
+		{
+			int count = 0;
+			foreach (string fileName in Directory.GetFiles (folder, "*.*", SearchOption.TopDirectoryOnly)) {
+				var fi = new FileInfo (fileName);
+				if ((fi.Attributes & FileAttributes.Hidden) != 0)
+					continue;
+				var archiveFileName = ArchiveNameForFile (fileName, folderInArchive);
+				long index = -1;
+				if (zip.ContainsEntry (archiveFileName, out index)) {
+					var e = zip.First (x => x.FullName == archiveFileName);
+					if (e.ModificationTime < fi.LastWriteTimeUtc)
+						zip.AddFile (fileName, archiveFileName, compressionMethod: method);
+				} else {
+					zip.AddFile (fileName, archiveFileName, compressionMethod: method);
+				}
+				count++;
+				if (count == ZipArchiveEx.ZipFlushLimit) {
+					Flush ();
+					count = 0;
+				}
+			}
+		}
+
+		public void RemoveFile (string folder, string file)
+		{
+			var archiveName = ArchiveNameForFile (file, Path.Combine (folder, Path.GetDirectoryName (file)));
+			long index = -1;
+			if (zip.ContainsEntry (archiveName, out index))
+				zip.DeleteEntry ((ulong)index);
+		}
+
+		public void AddDirectory (string folder, string folderInArchive, CompressionMethod method = CompressionMethod.Default)
+		{
+			if (!string.IsNullOrEmpty (folder)) {
+				folder = folder.Replace ('/', Path.DirectorySeparatorChar).Replace ('\\', Path.DirectorySeparatorChar);
+				folder = Path.GetFullPath (folder);
+				if (folder [folder.Length - 1] == Path.DirectorySeparatorChar) {
+					folder = folder.Substring (0, folder.Length - 1);
+				}
+			}
+
+			AddFiles (folder, folderInArchive, method);
+			foreach (string dir in Directory.GetDirectories (folder, "*", SearchOption.AllDirectories)) {
+				var di = new DirectoryInfo (dir);
+				if ((di.Attributes & FileAttributes.Hidden) != 0)
+					continue;
+				var internalDir = dir.Replace (folder, string.Empty);
+				string fullDirPath = folderInArchive + internalDir;
+				try {
+					zip.CreateDirectory (fullDirPath);
+				} catch (ZipException) {
+
+				}
+				AddFiles (dir, fullDirPath, method);
+			}
+		}
+
+		/// <summary>
+		/// HACK: aapt2 is creating zip entries on Windows such as `assets\subfolder/asset2.txt`
+		/// </summary>
+		public void FixupWindowsPathSeparators (Action<string, string> onRename)
+		{
+			bool modified = false;
+			foreach (var entry in zip) {
+				if (entry.FullName.Contains ('\\')) {
+					var name = entry.FullName.Replace ('\\', '/');
+					onRename?.Invoke (entry.FullName, name);
+					entry.Rename (name);
+					modified = true;
+				}
+			}
+			if (modified) {
+				Flush ();
+			}
+		}
+
+		public void Dispose ()
+		{
+			Dispose (true);
+			GC.SuppressFinalize (this);
+		}
+
+		protected virtual void Dispose (bool disposing)
+		{
+			if (disposing) {
+				if (zip != null) {
+					zip.Close ();
+					zip.Dispose ();
+					zip = null;
+				}
+			}
+		}
+	}
+}
---
+++ LibZipSharp.UnitTest/ZipArchiveExTest.cs
@@ -0,0 +1,152 @@
+﻿using NUnit.Framework;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using Xamarin.Tools.Zip;
+using Xamarin.Tools.Zip.Ex;
+
+namespace Tests {
+
+	public class ZipArchiveExTest {
+
+		Random rnd = new Random (234187614);
+
+		void CreateBaseFilesDirectory ()
+		{
+			if (Directory.Exists ("Resources"))
+				Directory.Delete ("Resources", recursive: true);
+			Directory.CreateDirectory ("Resources");
+			Directory.CreateDirectory (Path.Combine ("Resources", "subfolder"));
+			
+			for (int i = 0; i < 500; i++) {
+				int fileSize = rnd.Next (short.MaxValue*4);
+				byte [] buffer = new byte [fileSize];
+				rnd.NextBytes (buffer);
+				File.WriteAllBytes (Path.Combine ("Resources", $"File{i}.txt"), buffer);
+			}
+			for (int i = 0; i < 500; i++) {
+				int fileSize = rnd.Next (short.MaxValue);
+				byte [] buffer = new byte [fileSize];
+				rnd.NextBytes (buffer);
+				File.WriteAllBytes (Path.Combine ("Resources", "subfolder", $"FileSub{i}.txt"), buffer);
+			}
+		}
+
+		void CreateNewFilesDirectory ()
+		{
+			if (Directory.Exists ("NewFiles"))
+				Directory.Delete ("NewFiles", recursive: true);
+			Directory.CreateDirectory ("NewFiles");
+			Directory.CreateDirectory (Path.Combine ("NewFiles", "NewFiles1"));
+			for (int i = 0; i < 500; i++) {
+				int fileSize = rnd.Next (short.MaxValue * 4);
+				byte [] buffer = new byte [fileSize];
+				rnd.NextBytes (buffer);
+				File.WriteAllBytes (Path.Combine ("NewFiles", $"File{i}.txt"), buffer);
+			}
+			for (int i = 0; i < 500; i++) {
+				int fileSize = rnd.Next (short.MaxValue);
+				byte [] buffer = new byte [fileSize];
+				rnd.NextBytes (buffer);
+				File.WriteAllBytes (Path.Combine ("NewFiles", "NewFiles1", $"FileSub{i}.txt"), buffer);
+			}
+		}
+
+		CompressionMethod GetCompressionMethod (string fileName)
+		{
+			if (fileName.Contains ("5"))
+				return CompressionMethod.Store;
+			return CompressionMethod.Deflate;
+		}
+
+		[Test]
+		public void CreateaValidZipArchive ()
+		{
+			string package_base = "package_base.zip";
+			string temp = "package_base_new.zip";
+			if (File.Exists (package_base))
+				File.Delete (package_base);
+			if (File.Exists (temp))
+				File.Delete (temp);
+
+			CreateBaseFilesDirectory ();
+			using (var file = File.OpenWrite (package_base)) {
+				using (var zip = ZipArchive.Create (file)) {
+					zip.AddDirectory ("Resources", "res");
+				}
+			}
+			string extract_base = Path.GetFullPath ("extract_base");
+			if (Directory.Exists (extract_base))
+				Directory.Delete (extract_base, recursive: true);
+			Directory.CreateDirectory (extract_base);
+			using (var archive = ZipArchive.Open (package_base, FileMode.Open, strictConsistencyChecks: true)) {
+				archive.ExtractAll (extract_base);
+				foreach (var entry in archive) {
+					Assert.IsNotNull (entry, "Entry should not be null");
+					string destinationFile = Path.Combine (extract_base, entry.FullName);
+					if (entry.IsDirectory) {
+						DirectoryAssert.Exists (destinationFile, $"Directory {entry.FullName} should have been created.");
+					} else {
+						FileAssert.Exists (destinationFile, $"File {entry.FullName} should have been extracted.");
+						FileInfo info = new FileInfo (destinationFile);
+						Assert.AreEqual (info.Length, entry.Size, $"file size for {entry.FullName} ({entry.Size}) is not match {destinationFile} ({info.Length})");
+					}
+				}
+			}
+			File.Copy (package_base, temp, overwrite: true);
+			CreateNewFilesDirectory ();
+			using (var notice = Assembly.GetExecutingAssembly ().GetManifestResourceStream ("NOTICE.txt")) {
+				using (var zip = new ZipArchiveEx (temp, FileMode.Open)) {
+					zip.FixupWindowsPathSeparators ((a, b) => TestContext.WriteLine ($"Fixing up malformed entry `{a}` -> `{b}`"));
+					zip.Archive.AddEntry ("NOTICE", notice);
+
+					int count = 0;
+					string root = Path.GetFullPath ("NewFiles");
+					foreach (var file in Directory.GetFiles ("NewFiles", "*.txt")) {
+						zip.Archive.AddEntry (file.Replace (root, string.Empty), File.OpenRead (file), GetCompressionMethod (file));
+						if (count == ZipArchiveEx.ZipFlushLimit) {
+							zip.Flush ();
+							count = 0;
+						}
+					}
+				}
+			}
+			string extract_new = Path.GetFullPath ("extract_new");
+			if (Directory.Exists (extract_new))
+				Directory.Delete (extract_new, recursive: true);
+			Directory.CreateDirectory (extract_new);
+			using (var archive = ZipArchive.Open (temp, FileMode.Open, strictConsistencyChecks: true)) {
+				archive.ExtractAll (extract_new);
+				foreach (var entry in archive) {
+					Assert.IsNotNull (entry, "Entry should not be null");
+					string destinationFile = Path.Combine (extract_new, entry.FullName);
+					if (entry.IsDirectory) {
+						DirectoryAssert.Exists (destinationFile, $"Directory {entry.FullName} should have been created.");
+					} else {
+						FileAssert.Exists (destinationFile, $"File {entry.FullName} should have been extracted.");
+						FileInfo info = new FileInfo (destinationFile);
+						Assert.AreEqual (info.Length, entry.Size, $"file size for {entry.FullName} ({entry.Size}) is not match {destinationFile} ({info.Length})");
+						CompressionMethod expected = GetCompressionMethod (entry.FullName);
+						if (entry.FullName.StartsWith ("res", StringComparison.OrdinalIgnoreCase)) {
+							expected = entry.CompressionMethod;
+						}
+						Assert.AreEqual (expected, entry.CompressionMethod, $"{entry.FullName} was compressed with {entry.CompressionMethod} but should have been compressed as {expected}.");
+					}
+				}
+			}
+			if (Directory.Exists ("Resources"))
+				Directory.Delete ("Resources", recursive: true);
+			if (Directory.Exists ("NewFiles"))
+				Directory.Delete ("NewFiles", recursive: true);
+			if (Directory.Exists (extract_base))
+				Directory.Delete (extract_base, recursive: true);
+			if (Directory.Exists (extract_new))
+				Directory.Delete (extract_new, recursive: true);
+			if (File.Exists (package_base))
+				File.Delete (package_base);
+			if (File.Exists (temp))
+				File.Delete (temp);
+		}
+	}
+}
