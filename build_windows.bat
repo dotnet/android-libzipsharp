50fd865f87aedf6090f673039e8ae87a48b7d12d
Peter Collins <pecolli@microsoft.com>
2024-02-14 16:55:24 -0800

    Update conditions and cl/link flags

--- build_windows.bat
+++ build_windows.bat
@@ -19,9 +19,10 @@ set ARTIFACTS_DIR_ROOT_ARM64=%ARTIFACTS_DIR_ROOT%\winarm64
 set CONFIG=RelWithDebInfo
 set COMMON_CMAKE_PARAMS=-DCMAKE_BUILD_TYPE=%CONFIG% -G "Visual Studio 17 2022"
 
-set LINKER_PARAMS="/PROFILE /DYNAMICBASE /CETCOMPAT /guard:cf"
-set LINKER_PARAMS_ARM64="/PROFILE /DYNAMICBASE /guard:cf"
-set CXXFLAGS="/Qspectre /sdl /guard:cf"
+set CPARAMS=-DCMAKE_C_FLAGS_INIT="/Qspectre /sdl /guard:cf"
+set CXXPARAMS=-DCMAKE_CXX_FLAGS_INIT="/W3"
+set LINKER_PARAMS=-DCMAKE_EXE_LINKER_FLAGS_INIT="/PROFILE /DYNAMICBASE /CETCOMPAT /guard:cf"
+set LINKER_PARAMS_ARM64=-DCMAKE_EXE_LINKER_FLAGS_INIT="/PROFILE /DYNAMICBASE /guard:cf"
 
 echo Common cmake params: %COMMON_CMAKE_PARAMS%
 echo 32-bit dependencies artifacts dir: %ARTIFACTS_DIR_ROOT_32%
@@ -47,12 +48,13 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 REM 64-bit deps
 mkdir "%DEPS_BUILD_DIR_ROOT_64%"
 cmake %COMMON_CMAKE_PARAMS% ^
+	%CPARAMS% ^
+	%LINKER_PARAMS% ^
 	-B "%DEPS_BUILD_DIR_ROOT_64%" ^
 	-DVCPKG_TARGET_TRIPLET=x64-windows-static ^
 	-DBUILD_DEPENDENCIES=ON ^
 	"-DARTIFACTS_ROOT_DIR=%ARTIFACTS_DIR_ROOT_64%" ^
 	"-DCMAKE_INSTALL_PREFIX=%ARTIFACTS_DIR_ROOT_64%" ^
-	"-DCMAKE_EXE_LINKER_FLAGS_INIT=%LINKER_PARAMS%" ^
 	-A x64 .
 
 if %errorlevel% neq 0 exit /b %errorlevel%
@@ -66,12 +68,13 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 REM 64-bit arm deps
 mkdir "%DEPS_BUILD_DIR_ROOT_ARM64%"
 cmake %COMMON_CMAKE_PARAMS% ^
+	%CPARAMS% ^
+	%LINKER_PARAMS_ARM64% ^
 	-B "%DEPS_BUILD_DIR_ROOT_ARM64%" ^
 	-DVCPKG_TARGET_TRIPLET=arm-windows-static ^
 	-DBUILD_DEPENDENCIES=ON ^
 	"-DARTIFACTS_ROOT_DIR=%ARTIFACTS_DIR_ROOT_ARM64%" ^
 	"-DCMAKE_INSTALL_PREFIX=%ARTIFACTS_DIR_ROOT_ARM64%" ^
-	"-DCMAKE_EXE_LINKER_FLAGS_INIT=%LINKER_PARAMS_ARM64%" ^
 	-A arm64 .
 
 if %errorlevel% neq 0 exit /b %errorlevel%
@@ -85,12 +88,13 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 REM 32-bit deps
 mkdir "%DEPS_BUILD_DIR_ROOT_32%"
 cmake %COMMON_CMAKE_PARAMS% ^
+	%CPARAMS% ^
+	%LINKER_PARAMS% ^
 	-B "%DEPS_BUILD_DIR_ROOT_32%" ^
 	-DVCPKG_TARGET_TRIPLET=x86-windows-static ^
 	-DBUILD_DEPENDENCIES=ON ^
 	"-DARTIFACTS_ROOT_DIR=%ARTIFACTS_DIR_ROOT_32%" ^
 	"-DCMAKE_INSTALL_PREFIX=%ARTIFACTS_DIR_ROOT_32%" ^
-	"-DCMAKE_EXE_LINKER_FLAGS_INIT=%LINKER_PARAMS%" ^
 	-A Win32 .
 
 if %errorlevel% neq 0 exit /b %errorlevel%
@@ -105,12 +109,14 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 REM 64-bit library
 mkdir "%LIB_BUILD_DIR_ROOT_64%"
 cmake %COMMON_CMAKE_PARAMS% ^
+	%CPARAMS% ^
+	%CXXPARAMS% ^
+	%LINKER_PARAMS% ^
 	-B "%LIB_BUILD_DIR_ROOT_64%" ^
 	-DVCPKG_TARGET_TRIPLET=x64-windows-static ^
 	-DBUILD_LIBZIP=ON ^
 	"-DARTIFACTS_ROOT_DIR=%ARTIFACTS_DIR_ROOT_64%" ^
 	"-DCMAKE_INSTALL_PREFIX=%ARTIFACTS_DIR_ROOT_64%" ^
-	"-DCMAKE_EXE_LINKER_FLAGS_INIT=%LINKER_PARAMS%" ^
 	-A x64 .
 
 if %errorlevel% neq 0 exit /b %errorlevel%
@@ -121,12 +127,14 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 REM 64-bit arm library
 mkdir "%LIB_BUILD_DIR_ROOT_ARM64%"
 cmake %COMMON_CMAKE_PARAMS% ^
+	%CPARAMS% ^
+	%CXXPARAMS% ^
+	%LINKER_PARAMS_ARM64% ^
 	-B "%LIB_BUILD_DIR_ROOT_ARM64%" ^
 	-DVCPKG_TARGET_TRIPLET=arm-windows-static ^
 	-DBUILD_LIBZIP=ON ^
 	"-DARTIFACTS_ROOT_DIR=%ARTIFACTS_DIR_ROOT_ARM64%" ^
 	"-DCMAKE_INSTALL_PREFIX=%ARTIFACTS_DIR_ROOT_ARM64%" ^
-	"-DCMAKE_EXE_LINKER_FLAGS_INIT=%LINKER_PARAMS_ARM64%" ^
 	-A arm64 .
 
 if %errorlevel% neq 0 exit /b %errorlevel%
@@ -137,12 +145,14 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 REM 32-bit library
 mkdir "%LIB_BUILD_DIR_ROOT_32%"
 cmake %COMMON_CMAKE_PARAMS% ^
+	%CPARAMS% ^
+	%CXXPARAMS% ^
+	%LINKER_PARAMS% ^
 	-B "%LIB_BUILD_DIR_ROOT_32%" ^
 	-DVCPKG_TARGET_TRIPLET=x86-windows-static ^
 	-DBUILD_LIBZIP=ON ^
 	"-DARTIFACTS_ROOT_DIR=%ARTIFACTS_DIR_ROOT_32%" ^
 	"-DCMAKE_INSTALL_PREFIX=%ARTIFACTS_DIR_ROOT_32%" ^
-	"-DCMAKE_EXE_LINKER_FLAGS_INIT=%LINKER_PARAMS%" ^
 	-A Win32 .
 
 if %errorlevel% neq 0 exit /b %errorlevel%
