577147e54409b3c5509d5872aa7aaff8aef3bdce
Peter Collins <pecolli@microsoft.com>
2024-02-21 01:46:11 -0800

    [ci] Migrate to the 1ES template (#135)
    * [ci] Migrate to the 1ES template
    
    Context: https://aka.ms/1espt
    
    The build pipeline has been updated to extend the 1ES pipeline template,
    which will keep the pipeline up to date with the latest compliance and
    security requirements.
    
    Compliance tasks and scans will run automatically as part of artifact
    upload steps, which are now referred to as "outputs".  Template outputs
    have replaced all instances of the `PublishPipelineArtifact` task.
    
    * Use self template reference
    
    * Test 1es-sign-artifacts yaml branch
    
    * Set compiler/linker flags
    
    * Test removal of xz submodule
    
    * Update flags
    
    * Update flags
    
    * Test template pivot
    
    * Add scan suppressions
    
    * Use main templates branch
    
    * Import more suppressions
    
    * Bump to xz 5.4.6
    
    * Update .gdn
    
    * Use github.com/tukaani-project/xz
    
    * Update suppressions path
    
    * Update conditions and cl/link flags
    
    * Update cl/link flags
    
    * Update flags
    
    * Try a different way to pass args to submodules
    
    * Disable a warning we can't do much about
    
    * Update gdnsuppress:
    
    * Update build_windows, gdnsuppress
    
    * Update gdnsuppress
    
    * Update build images
    
    * Install latest 7.0 sdk for test lanes
    
    * Use latest 7.0 sdk
    
    ---------
    
    Co-authored-by: Marek Habersack <grendel@twistedcode.net>

--- build_windows.bat
+++ build_windows.bat
@@ -19,6 +19,12 @@ set ARTIFACTS_DIR_ROOT_ARM64=%ARTIFACTS_DIR_ROOT%\winarm64
 set CONFIG=RelWithDebInfo
 set COMMON_CMAKE_PARAMS=-DCMAKE_BUILD_TYPE=%CONFIG% -G "Visual Studio 17 2022"
 
+set CPARAMS_DEPS=-DCMAKE_C_FLAGS_INIT="/Qspectre /guard:cf /sdl /wd4996"
+set CPARAMS=-DCMAKE_C_FLAGS_INIT="/Qspectre /guard:cf /sdl"
+set CXXPARAMS=-DCMAKE_CXX_FLAGS_INIT="/W3"
+set LINKER_PARAMS=-DCMAKE_EXE_LINKER_FLAGS_INIT="/PROFILE /DYNAMICBASE /CETCOMPAT /guard:cf"
+set LINKER_PARAMS_ARM64=-DCMAKE_EXE_LINKER_FLAGS_INIT="/PROFILE /DYNAMICBASE /guard:cf"
+
 echo Common cmake params: %COMMON_CMAKE_PARAMS%
 echo 32-bit dependencies artifacts dir: %ARTIFACTS_DIR_ROOT_32%
 echo 64-bit dependencies artifacts dir: %ARTIFACTS_DIR_ROOT_64%
@@ -43,6 +49,9 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 REM 64-bit deps
 mkdir "%DEPS_BUILD_DIR_ROOT_64%"
 cmake %COMMON_CMAKE_PARAMS% ^
+	%CPARAMS_DEPS% ^
+	%CXXPARAMS% ^
+	%LINKER_PARAMS% ^
 	-B "%DEPS_BUILD_DIR_ROOT_64%" ^
 	-DVCPKG_TARGET_TRIPLET=x64-windows-static ^
 	-DBUILD_DEPENDENCIES=ON ^
@@ -61,6 +70,9 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 REM 64-bit arm deps
 mkdir "%DEPS_BUILD_DIR_ROOT_ARM64%"
 cmake %COMMON_CMAKE_PARAMS% ^
+	%CPARAMS_DEPS% ^
+	%CXXPARAMS% ^
+	%LINKER_PARAMS_ARM64% ^
 	-B "%DEPS_BUILD_DIR_ROOT_ARM64%" ^
 	-DVCPKG_TARGET_TRIPLET=arm-windows-static ^
 	-DBUILD_DEPENDENCIES=ON ^
@@ -79,6 +91,9 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 REM 32-bit deps
 mkdir "%DEPS_BUILD_DIR_ROOT_32%"
 cmake %COMMON_CMAKE_PARAMS% ^
+	%CPARAMS_DEPS% ^
+	%CXXPARAMS% ^
+	%LINKER_PARAMS% ^
 	-B "%DEPS_BUILD_DIR_ROOT_32%" ^
 	-DVCPKG_TARGET_TRIPLET=x86-windows-static ^
 	-DBUILD_DEPENDENCIES=ON ^
@@ -98,6 +113,9 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 REM 64-bit library
 mkdir "%LIB_BUILD_DIR_ROOT_64%"
 cmake %COMMON_CMAKE_PARAMS% ^
+	%CPARAMS% ^
+	%CXXPARAMS% ^
+	%LINKER_PARAMS% ^
 	-B "%LIB_BUILD_DIR_ROOT_64%" ^
 	-DVCPKG_TARGET_TRIPLET=x64-windows-static ^
 	-DBUILD_LIBZIP=ON ^
@@ -113,6 +131,9 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 REM 64-bit arm library
 mkdir "%LIB_BUILD_DIR_ROOT_ARM64%"
 cmake %COMMON_CMAKE_PARAMS% ^
+	%CPARAMS% ^
+	%CXXPARAMS% ^
+	%LINKER_PARAMS_ARM64% ^
 	-B "%LIB_BUILD_DIR_ROOT_ARM64%" ^
 	-DVCPKG_TARGET_TRIPLET=arm-windows-static ^
 	-DBUILD_LIBZIP=ON ^
@@ -128,6 +149,9 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 REM 32-bit library
 mkdir "%LIB_BUILD_DIR_ROOT_32%"
 cmake %COMMON_CMAKE_PARAMS% ^
+	%CPARAMS% ^
+	%CXXPARAMS% ^
+	%LINKER_PARAMS% ^
 	-B "%LIB_BUILD_DIR_ROOT_32%" ^
 	-DVCPKG_TARGET_TRIPLET=x86-windows-static ^
 	-DBUILD_LIBZIP=ON ^
